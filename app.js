f2e3b6facd66f02e0c4b981ffc0eb450992702d4
Rivaldo <rivaldo.alvesb36@gmail.com>
2019-01-18 02:20:34 -0300

    Configuração inicial de projeto

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const consign = require('consign');
+const app = express();
+
+app.set('views', path.join(__dirname, 'views')); //middlewares pasta views
+app.set('view engine', 'ejs');                   //middlewares template engine ejs
+app.use(express.static(path.join(__dirname, 'public'))); //middlewares arquivos estáticos(imagens,css,js)
+
+consign({})
+    .include('models')
+    .then('controllers')
+    .then('routes')
+    .into(app)
+;
+
+app.listen(3000, () => {
+    console.log('Ntalk no ar.');
+});
