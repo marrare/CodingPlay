d1b9136a91153b239ff32c416d8a347b842732b9
Rivaldo <rivaldo.alvesb36@gmail.com>
2019-02-22 01:32:27 -0300

    Correção dump do banco de dados

--- public/sql/codingplay.sql
+++ public/sql/codingplay.sql
@@ -1,77 +1,180 @@
-drop database codingplay;
+-- MySQL dump 10.13  Distrib 5.7.24, for Win32 (AMD64)
+--
+-- Host: localhost    Database: codingplay
+-- ------------------------------------------------------
+-- Server version	5.7.24
 
-create database codingplay;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-use codingplay;
+DROP DATABASE IF EXISTS 'codingplay';
+CREATE DATABASE codingplay;
+USE DATABASE codingplay;
 
-create table professor (
-    id int not null AUTO_INCREMENT,
-    matricula varchar(20) not null,
-    nome varchar(60) not null,
-    email varchar(40) not null,
-    senha varchar(100) not null,
-    situacao tinyint(1) default 1,
-    data_Alteracao timestamp default current_timestamp,
-    primary key(id)
-);
+--
+-- Table structure for table `aluno`
+--
 
-create table aluno (
-    id int not null AUTO_INCREMENT,
-    matricula varchar(20) not null,
-    nome varchar(60) not null,
-    email varchar(40) not null,
-    senha varchar(100) not null,
-    situacao tinyint(1) default 1,
-    pontuacao int default 0,
-    nivel tinyint(1) default 0,
-    data_Alteracao timestamp default current_timestamp,
-    primary key(id)
-);
+DROP TABLE IF EXISTS `aluno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `aluno` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `matricula` varchar(20) NOT NULL,
+  `nome` varchar(60) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `senha` varchar(100) NOT NULL,
+  `situacao` tinyint(1) DEFAULT '1',
+  `pontuacao` int(11) DEFAULT '0',
+  `nivel` tinyint(1) DEFAULT '0',
+  `data_Alteracao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table problema (
-    id int not null AUTO_INCREMENT,
-    id_Professor int not null,
-    nome_problema varchar(20) not null,
-    desc_Problema text(10000) not null,
-    dificuldade char(1) not null,
-    data_Alteracao timestamp default current_timestamp,
-    primary key(id),
-    foreign key (id_Professor) references Professor(id)
-);
-	
-create table gabarito (
-    id int not null AUTO_INCREMENT,
-    id_Problema int  not null,
-    desc_Gabarito text(10000) not null,
-    data_Alteracao timestamp default current_timestamp,
-    primary key (id),
-    foreign key (id_Problema) references Problema(id)
-);
+--
+-- Dumping data for table `aluno`
+--
 
-create table sessao (
-    id int  not null AUTO_INCREMENT,
-    id_Professor int not null,
-    id_Problema int not null,
-    nome_sessao varchar(20) not null,
-    tamanho_grupo tinyint not null,
-    tempo_rotacao tinyint not null,
-    hora_inicio timestamp not null,
-    hora_final timestamp not null,
-    hora_inicio_realizado timestamp,
-    hora_final_realizado timestamp,
-    data_Alteracao timestamp default current_timestamp,
-    primary key (id),
-    foreign key (id_Professor) references Professor(id),
-    foreign key (id_Problema) references Problema(id)
-);
+LOCK TABLES `aluno` WRITE;
+/*!40000 ALTER TABLE `aluno` DISABLE KEYS */;
+/*!40000 ALTER TABLE `aluno` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Password professor1234
-INSERT INTO `professor`(`matricula`, `nome`, `email`, `senha`) VALUES (034452,"professor","professor@gmail.com","db26588a2def92ed7e1e72ed8befe9a27af133542b2b088c6534489535badbff");
+--
+-- Table structure for table `gabarito`
+--
 
--- Insert de problemas temporários para teste
-INSERT INTO `problema`(`id_Professor`, `nome_problema`, `desc_Problema`, `dificuldade`) VALUES (1,'Soma idade','Criar uma variável idade, adicionar 20 ao seu valor e imprimi-la.','F');
-INSERT INTO `problema`(`id_Professor`, `nome_problema`, `desc_Problema`, `dificuldade`) VALUES (1,'Média','Encontrar a média da soma destes valores: 7,8,8,5,4','F');
+DROP TABLE IF EXISTS `gabarito`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gabarito` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_Problema` int(11) NOT NULL,
+  `desc_Gabarito` text NOT NULL,
+  `data_Alteracao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `id_Problema` (`id_Problema`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Insert de sessões temporários para teste
-INSERT INTO `sessao`(`id_Professor`, `id_Problema`, `nome_sessao`, `tamanho_grupo`, `tempo_rotacao`, `hora_inicio`, `hora_final`) VALUES (1,1,"Acamp Programing",6,8,'2019-02-08 19:00:00','2019-02-08 22:00:00'),(1,2,"Development",6,8,'2019-02-09 15:00:00','2019-02-09 17:00:00'),(1,2,"Academing",6,8,'2019-02-09 19:00:00','2019-02-09 21:00:00'),(1,1,"Testando",6,8,'2019-02-10 15:00:00','2019-02-09 17:00:00');
+--
+-- Dumping data for table `gabarito`
+--
 
+LOCK TABLES `gabarito` WRITE;
+/*!40000 ALTER TABLE `gabarito` DISABLE KEYS */;
+/*!40000 ALTER TABLE `gabarito` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `problema`
+--
+
+DROP TABLE IF EXISTS `problema`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `problema` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_Professor` int(11) NOT NULL,
+  `nome_problema` varchar(20) NOT NULL,
+  `desc_Problema` text NOT NULL,
+  `dificuldade` char(1) NOT NULL,
+  `data_Alteracao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `id_Professor` (`id_Professor`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `problema`
+--
+
+LOCK TABLES `problema` WRITE;
+/*!40000 ALTER TABLE `problema` DISABLE KEYS */;
+INSERT INTO `problema` VALUES (1,1,'Soma idade','Criar uma variável idade, adicionar 20 ao seu valor e imprimi-la.','F','2019-02-22 04:13:57'),(2,1,'Média','Encontrar a média da soma destes valores: 7,8,8,5,4','F','2019-02-22 04:13:57');
+/*!40000 ALTER TABLE `problema` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `professor`
+--
+
+DROP TABLE IF EXISTS `professor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `professor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `matricula` varchar(20) NOT NULL,
+  `nome` varchar(60) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `senha` varchar(100) NOT NULL,
+  `situacao` tinyint(1) DEFAULT '1',
+  `data_Alteracao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `professor`
+--
+
+LOCK TABLES `professor` WRITE;
+/*!40000 ALTER TABLE `professor` DISABLE KEYS / senha de professor é 'professor1234' */;
+INSERT INTO `professor` VALUES (1,'34452','professor','professor@gmail.com','db26588a2def92ed7e1e72ed8befe9a27af133542b2b088c6534489535badbff',1,'2019-02-22 04:13:57');
+/*!40000 ALTER TABLE `professor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sessao`
+--
+
+DROP TABLE IF EXISTS `sessao`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sessao` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_Professor` int(11) NOT NULL,
+  `id_Problema` int(11) NOT NULL,
+  `nome_sessao` varchar(20) NOT NULL,
+  `tamanho_grupo` tinyint(4) NOT NULL,
+  `tempo_rotacao` tinyint(4) NOT NULL,
+  `hora_inicio` timestamp NOT NULL,
+  `hora_final` timestamp NOT NULL,
+  `hora_inicio_realizado` timestamp NULL DEFAULT NULL,
+  `hora_final_realizado` timestamp NULL DEFAULT NULL,
+  `data_Alteracao` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `id_Professor` (`id_Professor`),
+  KEY `id_Problema` (`id_Problema`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sessao`
+--
+
+LOCK TABLES `sessao` WRITE;
+/*!40000 ALTER TABLE `sessao` DISABLE KEYS */;
+INSERT INTO `sessao` VALUES (1,1,1,'Acamp Programing',6,8,'2019-02-08 22:00:00','2019-02-09 01:00:00',NULL,NULL,'2019-02-22 04:13:57'),(2,1,2,'Development',6,8,'2019-02-09 18:00:00','2019-02-09 20:00:00',NULL,NULL,'2019-02-22 04:13:57'),(3,1,2,'Academing',6,8,'2019-02-09 22:00:00','2019-02-10 00:00:00',NULL,NULL,'2019-02-22 04:13:57'),(4,1,1,'Testando',6,8,'2019-02-10 18:00:00','2019-02-09 20:00:00',NULL,NULL,'2019-02-22 04:13:57');
+/*!40000 ALTER TABLE `sessao` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-02-22  1:25:33
