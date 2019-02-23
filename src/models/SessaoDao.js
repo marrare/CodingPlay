1f603f8a493e4806113b8a8d6dc71ef39b38a903
Rivaldo <rivaldo.alvesb36@gmail.com>
2019-02-23 16:35:22 -0300

    Filtro de sessão por nome de problema - página listar sessões

--- src/models/SessaoDao.js
+++ src/models/SessaoDao.js
@@ -3,31 +3,31 @@ function SessaoDao(connection){
 }
 
 SessaoDao.prototype.listaSessoes = function(callback){
-    this._connection.query("select *, DATE_FORMAT(hora_inicio,'%Y/%m/%d  %H:%i') AS hora_inicio, DATE_FORMAT(hora_final,'%Y/%m/%d  %H:%i') AS hora_final from sessao A LEFT JOIN professor B ON A.id_professor = B.id order by hora_inicio asc", callback);
+    this._connection.query("select A.tamanho_grupo,B.nome,C.nome_problema, DATE_FORMAT(hora_inicio,'%Y/%m/%d  %H:%i') AS hora_inicio, DATE_FORMAT(hora_final,'%Y/%m/%d  %H:%i') AS hora_final from sessao A INNER JOIN professor B INNER JOIN problema C ON A.id_professor = B.id AND A.id_problema = C.id order by hora_inicio asc", callback);
 }
 
 SessaoDao.prototype.filtrar = function(sessao, callback){
     
-    var nomeSessao = sessao.sessao != null ? sessao.sessao : "";
+    var nomeProblema = sessao.problema != null ? sessao.problema : "";
     var nomeProfessor = sessao.professor != null ? sessao.professor : "";
     var dificuldade = sessao.dificuldade != null ? sessao.dificuldade : "";
     
-    if(nomeSessao != "" && nomeProfessor == "" && dificuldade == "") {
-        this._connection.query("select *, DATE_FORMAT(hora_inicio,'%Y/%m/%d  %H:%i') AS hora_inicio, DATE_FORMAT(hora_final,'%Y/%m/%d  %H:%i') AS hora_final from sessao A LEFT JOIN professor B ON A.id_professor = B.id WHERE nome_sessao LIKE'%"+nomeSessao+"%' order by hora_inicio asc", callback);
-    } else if (nomeSessao == "" && nomeProfessor != "" && dificuldade == "") {
-        this._connection.query("select *, DATE_FORMAT(hora_inicio,'%Y/%m/%d  %H:%i') AS hora_inicio, DATE_FORMAT(hora_final,'%Y/%m/%d  %H:%i') AS hora_final from sessao A LEFT JOIN professor B ON A.id_professor = B.id WHERE nome LIKE'%"+nomeProfessor+"%' order by hora_inicio asc", callback);
-    } else if (nomeSessao == "" && nomeProfessor == "" && dificuldade != "") {
-        this._connection.query("select *, DATE_FORMAT(hora_inicio,'%Y/%m/%d  %H:%i') AS hora_inicio, DATE_FORMAT(hora_final,'%Y/%m/%d  %H:%i') AS hora_final from sessao A LEFT JOIN professor B ON A.id_professor = B.id WHERE id_problema IN (SELECT id FROM problema WHERE dificuldade = '"+dificuldade+"') order by hora_inicio asc", callback);
-    } else if (nomeSessao != "" && nomeProfessor != "" && dificuldade == "") {
-        this._connection.query("select *, DATE_FORMAT(hora_inicio,'%Y/%m/%d  %H:%i') AS hora_inicio, DATE_FORMAT(hora_final,'%Y/%m/%d  %H:%i') AS hora_final from sessao A LEFT JOIN professor B ON A.id_professor = B.id WHERE nome_sessao LIKE'%"+nomeSessao+"%' AND nome LIKE'%"+nomeProfessor+"%' order by hora_inicio asc", callback);
-    } else if (nomeSessao != "" && nomeProfessor == "" && dificuldade != "") {
-        this._connection.query("select *, DATE_FORMAT(hora_inicio,'%Y/%m/%d  %H:%i') AS hora_inicio, DATE_FORMAT(hora_final,'%Y/%m/%d  %H:%i') AS hora_final from sessao A LEFT JOIN professor B ON A.id_professor = B.id WHERE nome_sessao LIKE'%"+nomeSessao+"%' AND id_problema IN (SELECT id FROM problema WHERE dificuldade = '"+dificuldade+"') order by hora_inicio asc", callback);
-    } else if (nomeSessao == "" && nomeProfessor != "" && dificuldade != "") {
-        this._connection.query("select *, DATE_FORMAT(hora_inicio,'%Y/%m/%d  %H:%i') AS hora_inicio, DATE_FORMAT(hora_final,'%Y/%m/%d  %H:%i') AS hora_final from sessao A LEFT JOIN professor B ON A.id_professor = B.id WHERE nome LIKE'%"+nomeProfessor+"%' AND id_problema IN (SELECT id FROM problema WHERE dificuldade = '"+dificuldade+"') order by hora_inicio asc", callback);
-    } else if (nomeSessao != "" && nomeProfessor != "" && dificuldade != "") {
-        this._connection.query("select *, DATE_FORMAT(hora_inicio,'%Y/%m/%d  %H:%i') AS hora_inicio, DATE_FORMAT(hora_final,'%Y/%m/%d  %H:%i') AS hora_final from sessao A LEFT JOIN professor B ON A.id_professor = B.id WHERE nome_sessao LIKE'%"+nomeSessao+"%' AND nome LIKE'%"+nomeProfessor+"%' AND id_problema IN (SELECT id FROM problema WHERE dificuldade = '"+dificuldade+"') order by hora_inicio asc", callback);
-    } else if (nomeSessao == "" && nomeProfessor == "" && dificuldade == "") {
-        this._connection.query("select *, DATE_FORMAT(hora_inicio,'%Y/%m/%d  %H:%i') AS hora_inicio, DATE_FORMAT(hora_final,'%Y/%m/%d  %H:%i') AS hora_final from sessao A LEFT JOIN professor B ON A.id_professor = B.id order by hora_inicio asc", callback);
+    if(nomeProblema != "" && nomeProfessor == "" && dificuldade == "") {
+        this._connection.query("select A.tamanho_grupo,B.nome,C.nome_problema, DATE_FORMAT(hora_inicio,'%Y/%m/%d  %H:%i') AS hora_inicio, DATE_FORMAT(hora_final,'%Y/%m/%d  %H:%i') AS hora_final from sessao A INNER JOIN professor B INNER JOIN problema C ON A.id_professor = B.id AND A.id_problema = C.id WHERE nome_problema LIKE'%"+nomeProblema+"%' order by hora_inicio asc", callback);
+    } else if (nomeProblema == "" && nomeProfessor != "" && dificuldade == "") {
+        this._connection.query("select A.tamanho_grupo,B.nome,C.nome_problema, DATE_FORMAT(hora_inicio,'%Y/%m/%d  %H:%i') AS hora_inicio, DATE_FORMAT(hora_final,'%Y/%m/%d  %H:%i') AS hora_final from sessao A INNER JOIN professor B INNER JOIN problema C ON A.id_professor = B.id AND A.id_problema = C.id WHERE nome LIKE'%"+nomeProfessor+"%' order by hora_inicio asc", callback);
+    } else if (nomeProblema == "" && nomeProfessor == "" && dificuldade != "") {
+        this._connection.query("select A.tamanho_grupo,B.nome,C.nome_problema, DATE_FORMAT(hora_inicio,'%Y/%m/%d  %H:%i') AS hora_inicio, DATE_FORMAT(hora_final,'%Y/%m/%d  %H:%i') AS hora_final from sessao A INNER JOIN professor B INNER JOIN problema C ON A.id_professor = B.id AND A.id_problema = C.id WHERE id_problema IN (SELECT id FROM problema WHERE dificuldade = '"+dificuldade+"') order by hora_inicio asc", callback);
+    } else if (nomeProblema != "" && nomeProfessor != "" && dificuldade == "") {
+        this._connection.query("select A.tamanho_grupo,B.nome,C.nome_problema, DATE_FORMAT(hora_inicio,'%Y/%m/%d  %H:%i') AS hora_inicio, DATE_FORMAT(hora_final,'%Y/%m/%d  %H:%i') AS hora_final from sessao A INNER JOIN professor B INNER JOIN problema C ON A.id_professor = B.id AND A.id_problema = C.id WHERE nome_problema LIKE'%"+nomeProblema+"%' AND nome LIKE'%"+nomeProfessor+"%' order by hora_inicio asc", callback);
+    } else if (nomeProblema != "" && nomeProfessor == "" && dificuldade != "") {
+        this._connection.query("select A.tamanho_grupo,B.nome,C.nome_problema, DATE_FORMAT(hora_inicio,'%Y/%m/%d  %H:%i') AS hora_inicio, DATE_FORMAT(hora_final,'%Y/%m/%d  %H:%i') AS hora_final from sessao A INNER JOIN professor B INNER JOIN problema C ON A.id_professor = B.id AND A.id_problema = C.id WHERE nome_problema LIKE'%"+nomeProblema+"%' AND id_problema IN (SELECT id FROM problema WHERE dificuldade = '"+dificuldade+"') order by hora_inicio asc", callback);
+    } else if (nomeProblema == "" && nomeProfessor != "" && dificuldade != "") {
+        this._connection.query("select A.tamanho_grupo,B.nome,C.nome_problema, DATE_FORMAT(hora_inicio,'%Y/%m/%d  %H:%i') AS hora_inicio, DATE_FORMAT(hora_final,'%Y/%m/%d  %H:%i') AS hora_final from sessao A INNER JOIN professor B INNER JOIN problema C ON A.id_professor = B.id AND A.id_problema = C.id WHERE nome LIKE'%"+nomeProfessor+"%' AND id_problema IN (SELECT id FROM problema WHERE dificuldade = '"+dificuldade+"') order by hora_inicio asc", callback);
+    } else if (nomeProblema != "" && nomeProfessor != "" && dificuldade != "") {
+        this._connection.query("select A.tamanho_grupo,B.nome,C.nome_problema, DATE_FORMAT(hora_inicio,'%Y/%m/%d  %H:%i') AS hora_inicio, DATE_FORMAT(hora_final,'%Y/%m/%d  %H:%i') AS hora_final from sessao A INNER JOIN professor B INNER JOIN problema C ON A.id_professor = B.id AND A.id_problema = C.id WHERE nome_problema LIKE'%"+nomeProblema+"%' AND nome LIKE'%"+nomeProfessor+"%' AND id_problema IN (SELECT id FROM problema WHERE dificuldade = '"+dificuldade+"') order by hora_inicio asc", callback);
+    } else if (nomeProblema == "" && nomeProfessor == "" && dificuldade == "") {
+        this._connection.query("select A.tamanho_grupo,B.nome,C.nome_problema, DATE_FORMAT(hora_inicio,'%Y/%m/%d  %H:%i') AS hora_inicio, DATE_FORMAT(hora_final,'%Y/%m/%d  %H:%i') AS hora_final from sessao A INNER JOIN professor B INNER JOIN problema C ON A.id_professor = B.id AND A.id_problema = C.id order by hora_inicio asc", callback);
     }
     
 }
