14e1f50b2bd457f7a8a3679011415bcd83c9de3e
marrare <rivaldo.alvesb36@gmail.com>
2019-04-24 22:35:48 -0300

    Heroku mysql

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 
 var connMySQL = function(){
 	return mysql.createConnection({
-		host : 'localhost',
-		user : 'root',
-		password : '',
-		database : 'codingplay',
+		host : 'us-cdbr-iron-east-02.cleardb.net',
+		user : 'be7258b02a5e94',
+		password : '97f99d4d',
+		database : 'heroku_63ededd1d9adf97',
         charset: "utf8_general_ci"
 	});
 }
