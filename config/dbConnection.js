34f65021bca481780fb5128536d3221ffaab34ba
marrare <rivaldo.alvesb36@gmail.com>
2020-06-02 20:16:35 -0300

    Integracao do Google Analytics

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,10 +2,10 @@ var mysql = require('mysql');
 
 var connMySQL = function(){
 	return mysql.createConnection({
-		host : 'us-cdbr-iron-east-02.cleardb.net',
-		user : 'b68c33d5639188',
-		password : '38223393',
-		database : 'heroku_9236d8f17f82b9f',
+		host : 'localhost',
+		user : 'codingplay',
+		password : 'password',
+		database : 'codingplay',
         charset: "utf8_general_ci"
 	});
 }
